08702aa99077873ed2edd11217e20d3a4a07c18a
Guilherme <gprsilva2008@gmail.com>
2024-12-04 16:59:22 -0300

    Fazendo os radios para mudar o conteudo da tela financeuiro

--- public/js/financeiro.js
+++ public/js/financeiro.js
@@ -15,6 +15,14 @@
   }
 });
 
+function toggleSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section.style.display === "none" || section.style.display === "") {
+    section.style.display = "block";
+  } else {
+    section.style.display = "none";
+  }
+}
 
 
 
