3d82fc0ba013392b932a8dd5e5ff8c9715c54940
Arthur Américo <arthuramerico33@gmail.com>
2024-12-05 14:49:41 -0300

    atualização do filtro

--- public/js/catalogo.js
+++ public/js/catalogo.js
@@ -26,3 +26,26 @@ function abrirLinha() {
 
 }
 }
+
+function teste(){
+    alert('teste12234')
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const filtrarButton = document.querySelector("#filtrar");
+    const caixote = document.querySelector("#caixote");
+
+    //mostrar ou esconder o caixote
+    filtrarButton.addEventListener("click", (event) => {
+        caixote.classList.toggle("active");
+        event.stopPropagation(); // impede que o click no filtro feche o dropdown
+    });
+
+    // Fecha o caixote se clicar fora dele
+    document.addEventListener("click", (event) => {
+        if (!caixote.contains(event.target) && !filtrarButton.contains(event.target)) {
+            caixote.classList.remove("active");
+        }
+    });
+});
